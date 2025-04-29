8a1a4c68ed94c932c9657f1762915783080abd9f
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-29 08:12:13 +0700

    Menambahkan API movies

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Modul10;
+using System.Diagnostics.Tracing;
+
+namespace Modul10.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private static List<Movie> daftarFilm = new List<Movie>
+        {
+            new Movie
+            (
+                "The Shawshank Redemption",
+                "Frank Darabant",
+                ["Tim Robbins", "Morgan Freeman", "Bob Gurton", "William Sadler"],
+                "Two imprisoned men band over a number of years, finding solace and eventual redemption through acts of common decency."
+            ),
+
+            new Movie
+            (
+                "The Godfather",
+                "Francis Ford Coppola",
+                ["Marlon Brando", "Al Pacino", "James Caan", "Diane Keaton"],
+                "The aging patriarch of an organized crime dynasty in postwar New York City transfers control of his clandestine empire to his reluctant youngest son."
+
+            )
+        };
+
+
+        // GET: api/mahasiswa
+        [HttpGet]
+        public ActionResult<IEnumerable<Movie>> Get()
+        {
+            return (daftarFilm);
+        }
+
+        // POST: api/mahasiswa
+        [HttpPost]
+        public ActionResult<IEnumerable<Movie>> Post([FromBody] Movie film)
+        {
+            if (film == null)
+            {
+                return BadRequest(new { message = "Data film tidak valid." });
+            }
+
+            daftarFilm.Add(film);
+            return Ok(daftarFilm);
+        }
+
+        // GET: api/mahasiswa/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Movie> GetById(int id)
+        {
+            if (id < 0 || id >= daftarFilm.Count)
+                return NotFound(new { message = "Film tidak ditemukan." });
+
+            return Ok(daftarFilm.ElementAt(id));
+        }
+
+        // DELETE: api/mahasiswa/{id}
+        [HttpDelete("{id}")]
+        public ActionResult<IEnumerable<Movie>> Delete(int id)
+        {
+            if (id < 0 || id >= daftarFilm.Count)
+                return NotFound(new { message = "Film tidak ditemukan." });
+
+            daftarFilm.RemoveAt(id);
+            return Ok(daftarFilm);
+        }
+    }
+}
---
+++ Movie.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace Modul10
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Director {  get; set; }
+        public List<string> Stars {  get; set; }
+        public string Description { get; set; }
+
+        public Movie(string title, string director, List<string> stars, string Description)
+        {
+            this.Title = title;
+            this.Director = director;
+            this.Stars = stars;
+            this.Description = Description;
+        }
+    }
+}
